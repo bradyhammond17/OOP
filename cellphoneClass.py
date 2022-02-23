10bcdced2deb629ed11b849b073d267725aa48d2
Brady Hammond <brady_hammond1.baylor.edu>
2022-02-23 13:35:09 -0600

    fixed cell phone program

--- cellphoneClass.py
+++ cellphoneClass.py
@@ -1,27 +1,25 @@
-
 class Phone:
-
-    def __init__(self,manufact, model, price):
+    def __init__(self, manufact, model, price):
         self.__manufact = manufact
         self.__model = model
         self.__retail_price = price
 
-    #Accepts an argument for phone's manufacturer
+    # Accepts an argument for phone's manufacturer
 
     def set_manufact(self, manufact):
         self.__manufact = manufact
 
-    #Accepts argument for model
+    # Accepts argument for model
     def set_model(self, model):
         self.__model = model
 
-    #Accepts argument for price
+    # Accepts argument for price
     def set_retail_price(self, price):
         self.__retail_price = price
 
     def get_manufact(self):
         return self.__manufact
-    
+
     def get_model(self):
         return self.__model
 
